963d69faafb8ee226ff7194539c551a3f46b0eb7
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-07-18 01:35:11 -0700

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # C-Python-Module-Implementation
 A template to convert C / C++ code to a Python Module using pybind11
+
+https://pybind11.readthedocs.io/en/stable/index.html
--- src/module_binder.cpp
+++ src/module_binder.cpp
@@ -8,8 +8,8 @@ int binder_wrapper(int x, int y) {
     return your_c_function(x, y);
 }
 
-PYBIND11_MODULE(code_module, module_) {  // Module name is "code_module"
-    module_.doc() = "Module description";
-    module_.def("module_function_name", &binder_wrapper, "Function description");
+PYBIND11_MODULE(code_module, m) {  // Module name is "code_module"
+    m.doc() = "Module description";
+    m.def("module_function_name", &binder_wrapper, "Function description");
     // Add more functions
 }
